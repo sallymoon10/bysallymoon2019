fbf9a86793c16fe7c6c40a2ad37549aeb79853a4
Sally Moon <sallymoon10>
2019-11-02 02:21:14 -0400

    working on landing page

---
+++ app/components/Colors.js
@@ -0,0 +1,13 @@
+const white = '#FFFFFF';
+const blue =  '#26377F';
+const red =  '#EB5B55';
+const grey =  '#F3F3F3';
+
+const Colours = {
+    white,
+    blue,
+    red,
+    grey
+};
+
+export default Colours;
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -6,13 +6,66 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import styled from 'styled-components';
+import colors from 'components/Colors.js'
+
+const HomeContainer = styled.div`
+  width: 100%;
+`;
+
+const Header = styled.div`
+  flex: 2;
+  padding: 10%;
+  background-color: #ffffff;
+`;
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 5%;
+  justifyContent: center;
+  alignItems: center;
+`;
+
+const Image = styled.img`
+  width: 50%;
+  height: 50%;
+  padding-left: 5%;
+  padding-right: 5%;
+  flex: 1;
+  align-self: flex-end;
+`;
+
+const SectionImage = styled.img`
+  width: 80%;
+  height: 80%;
+  flex: 1;
+`;
+
+
 
 export default function HomePage() {
   return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
+      <HomeContainer>
+          <Header>
+
+          </Header>
+          <Section style={{backgroundColor: colors.white}}>
+              <Image src={require('../../images/Landing/ProfileCircle.png')}/>
+              <Image src={require('../../images/Landing/Intro.png')}/>
+          </Section>
+          <Section style={{backgroundColor: colors.red}}>
+              <SectionImage src={require('../../images/Landing/AILandingSection.png')}/>
+          </Section>
+          <Section style={{backgroundColor: colors.blue}}>
+              <SectionImage src={require('../../images/Landing/CodeLandingSection.png')}/>
+          </Section>
+          <Section style={{backgroundColor: colors.red}}>
+              <SectionImage src={require('../../images/Landing/DesignLandingSection.png')}/>
+          </Section>
+          <Section style={{backgroundColor: colors.white}}>
+              <SectionImage src={require('../../images/Landing/LetsWork.png')}/>
+          </Section>
+      </HomeContainer>
   );
 }
